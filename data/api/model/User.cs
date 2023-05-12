10af612b4b5c3e0d0ee6f9e649b9721d81a45d2f
ISPr24-39_BeluakovDS <dan.bel.89@bk.ru>
2023-05-13 01:12:24 +0400

    user add column house

--- data/api/model/User.cs
+++ data/api/model/User.cs
@@ -8,6 +8,7 @@
         public string Password { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
         public int Balance { get; set; }
+        public int Hourse { get; set; }
         public Specialization Specialization { get; set; } = new();
     }
 }
